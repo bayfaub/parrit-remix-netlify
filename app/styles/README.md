f49f94987e4018b016273abf2d8340da65bcbd74
Anthony Dreessen <anthonydreessen@gmail.com>
2024-01-13 12:27:15 -0800

    move project over (again)

---
+++ app/styles/README.md
@@ -0,0 +1,8 @@
+## Parrit Styles
+
+As of this writing, Parrit styles are generated using SASS.
+
+You can generate them using npm
+
+- `npm dev:css`
+- `npm build:css`
---
+++ app/ui/Button.tsx
@@ -0,0 +1,29 @@
+import "~/styles/inputs.css";
+
+import React from "react";
+
+interface Props {
+  name: string;
+  shortName?: string;
+  className?: string;
+  clickFunction?: VoidFunction;
+  type?: "button" | "submit" | "reset" | undefined;
+  tooltip?: string;
+}
+
+export const Button: React.FC<Props> = (props) => {
+  const type = props.type || "button";
+
+  return (
+    <button
+      type={type}
+      title={props.tooltip}
+      className={props.className}
+      onClick={props.clickFunction}
+      aria-label={props.name}
+    >
+      <span className="name">{props.name}</span>
+      <span className="short-name">{props.shortName || props.name}</span>
+    </button>
+  );
+};
